010dc5414a462f21ce484f389d1232f3f9fe7f4f
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-02-24 19:42:56 +0530

    Initial commit from Create Next App

---
+++ pages/api/hello.js
@@ -0,0 +1,11 @@
+let employees = [
+  {
+    id: 1,
+    first_name: "George",
+    last_name: "Bluth",
+    email: "george@gmail.com",
+    avatar: "https://reqres.in/img/faces/1-image.jpg",
+    clock_in: "2025-02-24T08:00:00Z", // Example clock-in time
+    clock_out: "2025-02-24T17:00:00Z", // Example clock-out time
+  },
+];
