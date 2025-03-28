8e1c33644a16839259cdd0d1eb5291027176c02a
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-03-28 11:21:28 +0530

    allowed cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://timehub-api.chaincodeconsulting.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
