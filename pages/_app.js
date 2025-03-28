7ecd66dd87da0c3094147da69dac4056959703d0
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-03-28 14:55:20 +0530

    updated-fevicon and Title

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,22 @@ import Layout from "../components/Layout";
 import { Toaster } from "react-hot-toast";
 import React from 'react';
 import { Provider } from "react-redux";
-import {store} from "../store";
+import { store } from "../store";
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-    <Layout>
-      <Toaster position="top-right" />
-      <Component {...pageProps} />
-    </Layout>
+      <Head>
+        <title>ChainCode Consulting - TimeHub</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        {/* If using PNG or SVG */}
+        <link rel="icon" type="image/png" href="/logo.png" />
+      </Head>
+      <Layout>
+        <Toaster position="top-right" />
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
-  )
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>ChainCode Consulting - TimeHub</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* If using PNG or SVG */}
+        <link rel="icon" type="image/png" href="/logo.png" />  
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
